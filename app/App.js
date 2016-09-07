ed2813ecddc4fa2571d18a8b18a68ea146f9ea65
danyflorez <agustin.florez@techandsolve.com>
2016-09-07 16:16:15 -0500

    first commit

--- app/App.js
+++ app/App.js
@@ -1,12 +1,49 @@
 import React, { Component } from 'react';
 import {render} from 'react-dom';
+import BankBalanceStore from './BankBalanceStore';
+import BankActions from './BankActions';
+import {Container} from 'flux/utils';
 
 class App extends Component {
+
+  constructor(){
+    super(...arguments);
+    BankActions.createAccount();
+    this.state = {
+      balance: BankBalanceStore.getState()
+    }
+  }
+
+  deposit(){
+    BankActions.depositIntoAccount(Number(this.refs.ammount.value));
+    this.refs.ammount.value = '';
+  }
+
+  withdraw(){
+    BankActions.withDrawFromAccount(Number(this.refs.ammount.value));
+    this.refs.ammount.value = '';
+  }
+
   render(){
     return (
-      <h1>Hello World</h1>
+      <div>
+        <header>FluxTrush Bank</header>
+        <h1>Your balance is ${(this.state.balance).toFixed(2)} </h1>
+        <div className="atm">
+          <input type="text" placeholder="Enter Ammount" ref="ammount" />
+          <br />
+          <button onClick={this.withdraw.bind(this)}> Withdraw </button>
+          <button onClick={this.deposit.bind(this)}> Deposit </button>
+        </div>
+      </div>
     );
   }
 }
 
-render(<App />, document.getElementById('root'));
+
+App.getStores = () => ([BankBalanceStore]);
+App.calculateState = (prevState) => ({balance: BankBalanceStore.getState()});
+
+const AppContainer = Container.create(App);
+
+render(<AppContainer />, document.getElementById('root'));
